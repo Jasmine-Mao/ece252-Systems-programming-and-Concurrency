78815093b3a3b6566132bb3dc0253b2014abbc8e
Jasmine Mao <j54mao@uwaterloo.ca>
2023-09-29 17:47:03 -0400

    added more stuff to find_png

--- lab1/png_fun/find_png.c
+++ lab1/png_fun/find_png.c
@@ -8,72 +8,60 @@
 #include<dirent.h>
 #include <unistd.h>
 
+void function(struct dirent *dirent_ptr){
+    // function that takes a directory as an argument
+    // goes through all files of the directory. 
+    //      if it finds a file, check if it's a png. if it is, print the file path
+    //                                               if not, move on to the next file
+
+    // if it comes across a directory, call the function again
+    // function returns when all files in a given directory have been checked
+
+    while(dirent_ptr != NULL){
+        if(dirent_ptr->d_type == 4){    // DIRECTORY
+            function(dirent_ptr);
+        }
+        else if(dirent_ptr->d_type == 8){   // REGULAR FILE
+            printf("regular file: %s\n", dirent_ptr->d_name);
+        }
+    }
+
+    // gets here once all files have been read through (dirent ptr is pointing to NULL)
+
+}
+
+
+
 int main(int argc, char* argv[]){
     /*findpng takes a directory name as an argument. only takes ONE argument*/
 
     /*VARIABLES*/
-    char *ptr;
-    struct stat buf;
     DIR *directory;
     struct dirent *dirent_ptr;
 
-
-     if(argc != 2){
-        /*CASE WHERE NO OR MULTIPLE DIRECTOY PATHS ARE PASSED*/
+    /* if(argc != 2){
+        // CASE WHERE NO OR MULTIPLE DIRECTOY PATHS ARE PASSED
         printf("ERROR, please enter one (1) argument.\n");
         exit(1);
-    }
+    }  */
+    printf("inputs (DELETE ME AFTER, CHANGE TO argv[1]!!!): ");
+    scanf("%[^\n]%*c", argv[0]);
 
-    /*IDENTIFY IF THIS IS A DIRECTORY OR NOT*/
-    if((directory = opendir(argv[1])) == NULL){
+    if((directory = opendir(argv[0])) == NULL){
         // IF THE DIRECTORY PASSED IS NOT A DIRECTORY, EXIT WITH ERROR STATUS
-        printf("Arg passed is not a directory. THIS NEEDS TO BE CHANGED TO 'No PNG found'\n");
+        printf("Arg passed is not a directory. THIS NEEDS TO BE CHANGED TO 'No PNG file found'\n");
         exit(1);
     }
 
-    /*REACH HERE IF THE DIRECTORY PASSED IS A REAL DIRECTORY
-      
-      FUNCTION TO FIND ALL PNGS WILL ONLY TAKE INTO ACCOUNT WHETHER THE FIRST 8 BYTES 
-      IDENTIFY THE FILE AS A PNG. NO CORRUPTION DETECTION WILL BE MADE
-      
-      BASE CASE: NO SUBDIRECTORIES, GET ALL FILES WITH THE .PNG EXTENSION AND CHECK IF IT'S A PNG
-      
-      OTHER CASES: SEARCH OTHER SUBDIRECTORIES. THEN, ONCE RETURNED, LOOK THROUGH THE PNG'S TO SEE IF ACTUALLY PNG*/
-
-    while((dirent_ptr = readdir(directory)) != NULL){
-        /*char *str_path = dirent_ptr->d_name;
-
-        if (str_path == NULL) {
-            fprintf(stderr,"Null pointer found!"); 
-            exit(3);
-        } else {
-            printf("%s\n", str_path);
-        } */
+    /* while((dirent_ptr = readdir(directory)) != NULL){
+        // PRINTS ALL FILES AND SUBDIRECTORIES GIVEN THE DIRECTORY PATH
         printf("%s\n", dirent_ptr->d_name);
-    }
-    closedir(directory);
-    
-    
-    
-    
-    /* lstat(argv[1], &buf);
-    if(S_ISDIR(buf.st_mode)){
-        // if passed is a directory
-        printf("DIRECTORY FOUND!\n");
-        if((directory = opendir(argv[1]))){
-
-        }
-    }
-    else{
-        // if path passed is not a directory
-        printf("No PNG file found\n");
-        exit(2);
     } */
 
+    function(dirent_ptr = readdir(directory));
 
+    closedir(directory);
 
-
-    /**/
 
     return 0;
 }
