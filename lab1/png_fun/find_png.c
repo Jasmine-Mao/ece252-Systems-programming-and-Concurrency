300910f6dfddaaadc3ab7dd7962e780f0a846ff3
Jasmine Mao <j54mao@uwaterloo.ca>
2023-09-30 16:27:13 -0400

    when function finds a file ending with .png or .PNG, looks through the first 4 bytes and sees if they match the png signature.
    Does not look if the png is corrupted or anything, just if it matches the png signiture

--- lab1/png_fun/find_png.c
+++ lab1/png_fun/find_png.c
@@ -8,62 +8,6 @@
 #include<dirent.h>
 #include <unistd.h>
 
-void function(DIR *directory, char filePath[]){
-    // function that takes a directory as an argument
-    // goes through all files of the directory. 
-    //      if it finds a file, check if it's a png. if it is, print the file path
-    //                                               if not, move on to the next file
-
-    // if it comes across a directory, call the function again
-    // function returns when all files in a given directory have been checked
-    printf("%s\n", filePath);
-    if(directory != NULL){
-        struct dirent *dirent_pointer;
-        dirent_pointer = readdir(directory);
-
-        while((dirent_pointer = readdir(directory)) != NULL){
-            if(strcmp(dirent_pointer->d_name, "..") != 0){
-                // SKIPS OVER THE FIRST '..' DIRECTORY IN EACH SUBDIRECTORY --> NO GOOD INFO IN HERE
-                if(dirent_pointer->d_type == 4){    
-                    // FOUND A NEW DIRECTORY! CREATE A NEW DIR OBJECT AND CALL THE FUNCTION AGAIN
-                        char newPath[strlen(filePath)];
-                        strcpy(newPath, filePath);
-                        strcat(newPath, dirent_pointer->d_name);
-                        strcat(newPath, "/");
-
-                    DIR *newDirectory = opendir(newPath);
-                    if(newDirectory != NULL){
-                        
-                        function(newDirectory, newPath);
-                    }
-                }
-                else if(dirent_pointer->d_type == 8){
-                    // IF THE FILE IS A REGULAR FILE. NOW WE NEED TO CHECK IF IT'S A PNG
-
-                    // BEGIN BY CHECKING THE NAME OF THE FILE(ENDING IN .png)
-                    // find the last '.' in the file name, then see if the last 3 characters after the dot are 'png'
-                    
-                    int fileLength = strlen(dirent_pointer->d_name);
-                    if(fileLength > 4){
-                        // if file length at least contains enough stuff for '.png'
-                        const char* lastFour = &dirent_pointer->d_name[fileLength - 4];
-                        // get a pointer to the last 
-                        if((strcmp(lastFour, ".png") == 0) || (strcmp(lastFour, ".PNG") == 0)){
-                            // if the last 4 characters end with '.png' or '.PNG', as per the lab manual
-                            char temp[strlen(filePath)];
-                            strcpy(temp, filePath);
-                            strcat(temp, dirent_pointer->d_name);
-                            printf("%s\n", temp);
-                        }
-                    }
-                }
-                
-            }
-        }
-        closedir(directory);
-    }
-}
-
 int is_png(const char *fpath){
     // ensure file is valid type (regular) 
     // NOTE @jasmine: if you would rather redo this part in find_png
@@ -82,15 +26,75 @@ int is_png(const char *fpath){
 
     fread(read_buffer, sizeof(read_buffer), 1, f);
 
-    if (read_buffer[0] != 0x89 || 
-        read_buffer[1] != 0x50 ||
-        read_buffer[2] != 0x4E || 
-        read_buffer[3] != 0x47) {
+    if (/* (read_buffer[0] != 0x89) ||*/ 
+        (read_buffer[1] != 0x50) ||
+        (read_buffer[2] != 0x4E) || 
+        (read_buffer[3] != 0x47)) {
         return -1;
     }
 
     return 0;
 }
+
+void function(DIR *directory, char filePath[]){
+    // function that takes a directory as an argument
+    // goes through all files of the directory. 
+    //      if it finds a file, check if it's a png. if it is, print the file path
+    //                                               if not, move on to the next file
+
+    // if it comes across a directory, call the function again
+    // function returns when all files in a given directory have been checked
+
+
+    // printf("%s\n", filePath);
+    if(directory != NULL){
+        struct dirent *dirent_pointer;
+        dirent_pointer = readdir(directory);
+
+        while((dirent_pointer = readdir(directory)) != NULL){
+            if((strcmp(dirent_pointer->d_name, "..") != 0)&&(strcmp(dirent_pointer->d_name, "."))){
+                // SKIPS OVER THE FIRST '..' DIRECTORY IN EACH SUBDIRECTORY --> NO GOOD INFO IN HERE
+                if(dirent_pointer->d_type == 4){    
+                    // FOUND A NEW DIRECTORY! CREATE A NEW DIR OBJECT AND CALL THE FUNCTION AGAIN
+                        char newPath[strlen(filePath)];
+                        strcpy(newPath, filePath);
+                        strcat(newPath, dirent_pointer->d_name);
+                        strcat(newPath, "/");
+
+                        DIR *newDirectory = opendir(newPath);
+                        if(newDirectory != NULL){
+                            function(newDirectory, newPath);
+                        }
+                }
+                else if(dirent_pointer->d_type == 8){
+                    // IF THE FILE IS A REGULAR FILE. NOW WE NEED TO CHECK IF IT'S A PNG
+
+                    // BEGIN BY CHECKING THE NAME OF THE FILE(ENDING IN .png)
+                    int fileLength = strlen(dirent_pointer->d_name);
+                    if(fileLength > 4){
+                        // if file length at least contains enough stuff for '.png'
+                        const char* lastFour = &dirent_pointer->d_name[fileLength - 4];
+                        // get a pointer to the last 
+                        if((strcmp(lastFour, ".png") == 0) || (strcmp(lastFour, ".PNG") == 0)){
+                            // if the last 4 characters end with '.png' or '.PNG', as per the lab manual
+                            char temp[strlen(filePath)];
+                            strcpy(temp, filePath);
+                            strcat(temp, dirent_pointer->d_name);
+                            if(is_png(temp) == 0){
+                                printf("PNG!\n");
+                                printf("%s\n", temp);
+                            }
+                        }
+                    }
+                }
+                
+            }
+        }
+        closedir(directory);
+    }
+}
+
+
 
 int main(int argc, char* argv[]){
     /*findpng takes a directory name as an argument. only takes ONE argument*/
