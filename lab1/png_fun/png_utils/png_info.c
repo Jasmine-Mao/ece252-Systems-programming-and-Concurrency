467484806984d3e4eaebac3a79b95706cc51b468
Evelyn Campbell <e25campb@uwaterloo.ca>
2023-10-01 23:17:33 -0400

    amend png_info to match find_png uses

--- lab1/png_fun/png_utils/png_info.c
+++ lab1/png_fun/png_utils/png_info.c
@@ -1,37 +1,41 @@
-#include <sys/types.h>
-#include <sys/stat.h>
+/**
+ * @brief   functions for grabbing png information
+ * Authored by Evelyn; modified by Jasmine.
+ */
+
+// #include <sys/types.h>
+// #include <sys/stat.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "png_info.h"
 
 int is_png(const char *fpath){
-    struct stat type_buffer;
-    if (lstat(fpath, &type_buffer) < 0) {
-        perror("lstat error");
-        return -1;
-    } 
-    if (!S_ISREG(type_buffer.st_mode)) {
-        fprintf(stderr, "%s: Not a regular file", fpath);
-        return -1;
-    }
     
+    // struct stat type_buffer;                                     <-- remove later, going to use this logic to ensure the files arent broken when we use dis for
+    // if (lstat(fpath, &type_buffer) < 0) {                            catpng, and so it can exit gracefully
+    //     perror("lstat error");
+    //     return -1;
+    // } 
+    // if (!S_ISREG(type_buffer.st_mode)) {
+    //     fprintf(stderr, "%s: Not a regular file", fpath);
+    //     return -1;
+    // }
+
     FILE *f = fopen(fpath, "r");
     if (!f) {
         perror("fopen");
         return -1;
     }
 
-    char * read_buffer = malloc(4);     // input buffer for reading 4 bytes
-
+    char *read_buffer = malloc(4);
     fread(read_buffer, sizeof(read_buffer), 1, f);
 
-    if (read_buffer[0] != 0x89 || 
-        read_buffer[1] != 0x50 ||
-        read_buffer[2] != 0x4E || 
-        read_buffer[3] != 0x47) {
+    // verify bytes 2-4 make up correct png signature
+    if ((read_buffer[1] != 0x50) ||
+        (read_buffer[2] != 0x4E) ||
+        (read_buffer[3] != 0x47)) {
         return -1;
     }
-
     return 0;
 }
 
@@ -46,19 +50,3 @@ int is_png(const char *fpath){
 // int get_png_data_IHDR(struct data_IHDR *out, FILE *fp, long offset, int whence){
 
 // }
-
-// REMOVE LATER, using to test png_info
-int main(int argc, char *argv[]) 
-{
-    if (argc != 2){
-        printf("WRONG");
-        exit(1);
-    }
-    if (is_png(argv[1]) < 0){
-        printf("%s : Not a PNG file", argv[1]);
-        exit(1);
-    }
-    printf("yay it;s a png (print dimensions later)");
-    exit(1);
-
-}
