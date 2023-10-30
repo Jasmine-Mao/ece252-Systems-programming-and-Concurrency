d90c053c1d803995d0d557eb458a1a98cb54fd77
Evelyn Campbell <e25campb@uwaterloo.ca>
2023-10-30 14:53:44 -0400

    Add dependant files, structure paster2

---
+++ lab3/png_fun/paster2.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+
+#include "png_utils/cat_png.h"
+
+
+// likely we still want a curl header and data callback function, but the data one will use the ENTIRE PNG
+
+int consumer_protocol(){
+
+    // this is where the consumer parses the png data (from our buffer) for the idat data, inflates and stores it
+
+    // we can loop here
+
+    // critical section somewhere here
+
+    return 0;
+}
+
+int producer_protocol(){
+
+    //this is where we wanna curl and store pngs in our buffer
+
+    // we can loop here
+
+    // critical section somewhere here
+    
+    return 0;
+}
+
+int run_proccesses(int producer_count, int consumer_count){
+
+    for (int i = 0; i < producer_count; i++){
+        // if pid == child,
+        // lets break then call producer_protocol 
+        // or the reverse order
+        // if the process is a parent we wanna keep looping
+    }
+
+    for (int i = 0; i < consumer_count; i++){
+        // if pid == child,
+        // lets break then call consumer_protocol 
+        // or the reverse order
+        // if the process is a parent we wanna keep looping
+    }
+
+    // if we are the parent at this point, we should be ready to roll (create the png)
+
+    return 0;
+}
+
+int main(int argc, char * argv[]){
+    int c;
+
+    // PARSE ARGS:
+    // P, C passed to run_processes function
+    // B, X, N are set into global variables for buffer size, sleep time, and image number.
+    
+
+
+    return 0;
+}
