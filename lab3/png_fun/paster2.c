3a9f4ff9727afbf30424576fe60e04c7fd6daeb7
Jasmine Mao <j54mao@uwaterloo.ca>
2023-11-07 19:14:06 -0500

    merge errors

--- lab3/png_fun/paster2.c
+++ lab3/png_fun/paster2.c
@@ -9,10 +9,6 @@
 #include <semaphore.h>
 #include <unistd.h>
 
-#include <stdint.h>
-#include <stdbool.h>
-#include "png_utils/zutil.h"
-
 #include "png_utils/cat_png.h"
 #include "ring_buffer.h"
 #include "paster2.h"
@@ -21,17 +17,12 @@
 #define URL_2           "http://ece252-2.uwaterloo.ca:2530/image?img="
 #define URL_3           "http://ece252-3.uwaterloo.ca:2530/image?img="
 #define URL_IMAGE_SEG   "&part="
-
-#define SEM_PROC 1
 #define ECE_252_HEADER  "X-Ece252-Fragment: "
 #define SEM_PROC        1
 
 int BUFFER_SIZE;
 int SLEEP_TIME;
 int IMG_NUM;
-
-atomic_bool check_img[50] = {false};
-int num_fetched = 0;
 
 sem_t * sem_items;
 sem_t * sem_spaces;
@@ -42,7 +33,7 @@ size_t data_write_callback(char* recv, size_t size, size_t nmemb, void *userdata
     size_t real_size = size * nmemb;
 
     DATA_BUF* strip_data = (DATA_BUF*)userdata;
-    if(strip_data->size + real_size +1 > strip_data->max_size){
+    if(size > 10000){
         return CURLE_WRITE_ERROR;
     }
 
@@ -63,106 +54,24 @@ size_t header_write_callback(char* recv, size_t size, size_t nmemb, void* userda
     return real_size;
 }
 
-int consumer_protocol(void* arg, int sleeptime){ //iman
-    //implement while loop somewhere, cond while num_fetched < number of segments
-    sem_t* access = (sem_t*)arg;
-    IMG_BUF idat_holder; //temp buffer
-    IMG_BUF original_img;
-    original_img.buf = malloc(10000); //max size of img
-    
-    while (num_fetched != 50){
-    idat_holder.buf = NULL;
-    idat_holder.seq = 0;
-    idat_holder.size = 0;
-    idat_holder.write_success = -1;
-
-    sem_wait(access);
-    /*critical section: access image, clear queue slot, exit*/
-    //original_img.buf = image data //idat_holder will hold length of compressed idat
-    idat_holder.seq = 0; //seq number from buffer
-    sem_post(access);
-
-    usleep(sleeptime);
-
-    if (!check_img[idat_holder.seq]){
-        /* extract idat */
-        int read_index = PNG_SIG_SIZE + IHDR_CHUNK_SIZE;
-        unsigned int size_buf = 0;
-        memcpy(&size_buf, original_img.buf + read_index, CHUNK_LEN_SIZE);
-        size_buf = ntohl(size_buf);
-
-        idat_holder.size = (size_t)size_buf;
-
-        read_index += CHUNK_LEN_SIZE + CHUNK_TYPE_SIZE;
-        idat_holder.buf = malloc(idat_holder.size);
-
-        memcpy(idat_holder.buf, original_img.buf + read_index, idat_holder.size);
-
-        /* store idat */
-        int store_index = idat_holder.seq * STRIP_HEIGHT * (PNG_WIDTH * 4 + 1);
-        U64 inf_size;
-        mem_inf(idat_data + store_index, &inf_size, idat_holder.buf, idat_holder.size);
-
-        idat_holder.write_success = 0;
-        check_img[idat_holder.seq] = true;
-        free(idat_holder.buf);
-        num_fetched++;
-    }
-    }
-    free(original_img.buf);
-    return 0;
-}
-    
+int consumer_protocol(){ //iman
     /*steps*/
     /*1. access shared memory (buffer)*/
     /*2. fetch image segments in order*/
-        //set up a structure (array etc. check lab 2) to put image data fetched from buffer into -- consumers get image segment, identify image segment number, and put into respective memory slot
-        //once all image segments have been found, continue onto concatenate
     /*3. inflate and concatenate all*/
     /*4. deflate once all have been concatenated*/
 
-    /*
-    take the current queued item, (head of queue)
-    this item is a png binary
-        find idat_length
-        create temp buffer of size idat_length
-        store idat data into this buffer
-        inflate
-        store into global shared memory, in the proper index (grabbed from header in producer)
-        
-    curl easy perform
-    147 in paster.c
-    stored index
-    */
+    //have to manage multiple consumers (child processes)
+    // this is where the consumer parses the png data (from our buffer) for the idat data, inflates and stores it
 
-int write_file(const char *path, const void *in, size_t len)
-{
-    FILE *fp = NULL;
+    // we can loop here
 
-    if (path == NULL) {
-        fprintf(stderr, "write_file: file name is null!\n");
-        return -1;
-    }
+    // critical section somewhere here
 
-    if (in == NULL) {
-        fprintf(stderr, "write_file: input data is null!\n");
-        return -1;
-    }
-
-    fp = fopen(path, "wb");
-    if (fp == NULL) {
-        perror("fopen");
-        return -2;
-    }
-
-    if (fwrite(in, 1, len, fp) != len) {
-        fprintf(stderr, "write_file: incomplete write!\n");
-        return -3; 
-    }
-    return fclose(fp);
+    return 0;
 }
 
-int producer_protocol(int process_number, int num_processes){ //does this need the buffer access semaphore passed to it
+int producer_protocol(int process_number, int num_processes){
     /*----CURL SETUP----*/
     CURL* curl_handle = curl_easy_init();
 
@@ -221,28 +130,25 @@ int producer_protocol(int process_number, int num_processes){ //does this need t
 
         if((res == CURLE_OK)){
             printf("made it\n");
-            sprintf(fname, "./output_%d.png", process_number);
-            write_file(fname, strip_data.png_data, strip_data.size);
+            sem_wait(sem_spaces);
+            sem_wait(sem_lock);
+
+            // add to buffer
+            
+
+            sem_post(sem_lock);
+            sem_post(sem_items);
+
+            printf("out of crit sect\n");
         }
 
         process_number += num_processes;
 
         free(seg);
-
-        usleep(SLEEP_TIME * 100);
-        break;
     }
-    // this is where we wanna curl and store pngs in our buffer
-    // if there is no space in the buffer, do nothing
-
-    // smeaphore to access the buffer as a producer bc only one producer allowed in the buffer at a time
-    // we can loop here
-
-    // critical section somewhere here
     curl_easy_cleanup(curl_handle);
     return 0;
 }
-
 
 int run_processes(int producer_count, int consumer_count){
     int ring_buf_size = sizeof(RING_BUFFER) + (sizeof(DATA_BUF) * BUFFER_SIZE);
@@ -332,31 +238,21 @@ int main(int argc, char * argv[]){
     // ./paster2 B P C X N
 
     // P, C passed to run_processes function
-    // B, X, N are set into global variables for buffer size, sleep time, and image number.;
+    // B, X, N are stored in global variables BUFFER_SIZE, SLEEP_TIME, IMG_NUM
     if (argc != 6){
         fprintf(stderr, "Incorrect arguments!\n");
         return -1;
     }
     BUFFER_SIZE = atoi(argv[1]);
-    int p = atoi(argv[2]);
-    int c = atoi(argv[3]);
     SLEEP_TIME = atoi(argv[4]);
     IMG_NUM = atoi(argv[5]);
 
+    int p = atoi(argv[2]);
+    int c = atoi(argv[3]);
+
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+
     run_processes(p, c);
-
-    /*int c;
-    int x = 0;
-    curl_global_init(CURL_GLOBAL_DEFAULT);  // init curl stuff
-    
-    sem_t* buffer_spaces;
-    sem_t* buffer_items;
-
-    sem_t buffer_permission;
-    sem_init(&buffer_permission, 0, 1);
-    producer_protocol(3, 50, 11, 1);
-    consumer_protocol(&buffer_permission, x);
-    sem_destroy(&buffer_permission);*/
 
     return 0;
 }
