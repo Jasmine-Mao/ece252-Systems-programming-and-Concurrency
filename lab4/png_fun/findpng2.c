73355a234f845a145e5e043ec8325740b9b33fbc
Evelyn Campbell <e25campb@uwaterloo.ca>
2023-11-15 03:46:47 -0500

    Thread creation and writing results

--- lab4/png_fun/findpng2.c
+++ lab4/png_fun/findpng2.c
@@ -1,6 +1,7 @@
 // from starter code
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdatomic.h>
 #include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -16,9 +17,12 @@
 #include "findpng2.h"
 #include "frontier_stack.h"
 
+#define MAX_URLS 500
+
 // GLOBAL VARIABLES
 FRONTIER * urls_frontier;
 char ** unique_pngs;    // this is a pointer to an array of pointers (where each item is a pointer to a char array (string)
+int png_count = 0;
 
 // TODO: @<JASMINE> curl callbacks!
 
@@ -44,7 +48,7 @@ int ht_add_url(char * url){
 }
 
 // TODO: @<JASMINE> thread routine
-int visit_url(void * arg){
+void *visit_url(void * arg){
 
     
     // while frontiers_counter is non zero or while our png counter < M <-- IMPORTANT: im not sure if this implementation is sound! 
@@ -58,6 +62,37 @@ int visit_url(void * arg){
     // for pngs, lets add to the unique_pngs array
 
     // decrement frontiers counter (as we have now visited the url)
+
+    return NULL;
+}
+
+int write_results(char * logfile_name){
+    FILE *png_urls = fopen("png_urls.txt", "w");
+    if (!png_urls) {
+        perror("fopen");
+        fclose(png_urls);
+        return -1;
+    }
+    
+    if (png_count == 0){
+        fclose(png_urls);
+    }
+    else {
+        for (int i = 0; i < png_count; i++){
+            fprintf(png_urls, "%s\n", unique_pngs[i]);    // small but maybe wanna confirm if we can have a new line at the end of this file lol
+        }
+        fclose(png_urls);
+    }
+
+    if (logfile_name != NULL){
+        FILE *logfile = fopen(logfile_name, "w");
+        for (int i = 0; i < MAX_URLS; i++){
+            /*if (hashtable[i] != NULL){        // this inefficient, easy workaround is just to create a seperate container (array) w/ an end index
+                fprintf(logfile, "%s\n", hashtable[i]);  // variable, and add to it everytime we find a unique url. if this causes time influence @ the end we can do the workaround
+            }*/
+        }
+        fclose(logfile);
+    }
 
     return 0;
 }
@@ -75,7 +110,7 @@ int main(int argc, char * argv[]){
     int c;
     int num_threads = 1;
     int num_pngs = 50;
-    FILE* log_file;
+    char* logfile_name = NULL;
     // default to 1 thread looking for 50 pngs; no logging
 
     if(argc < 2){
@@ -107,7 +142,7 @@ int main(int argc, char * argv[]){
         case 'v':
             //memcpy(log_file, optarg, strlen(optarg));
             printf("file to write log to: %s\n", optarg);
-            log_file = fopen(optarg, "w+");
+            logfile_name = optarg;
             // creates a file for logging; ***must be closed at the end of any operation using the log
             break;
         default:
@@ -125,14 +160,35 @@ int main(int argc, char * argv[]){
     // initialize conditional variables, eg. frontiers_counter etc.
 
 
-    // TODO: @<name_here> thread creation, joining and cleanup
+    // TODO: @EVELYN thread creation, joining and cleanup
     // if t == 1, just jump to visit_url [seed]
     // else, create threads, visit_url will be our thread routine
     // threads join, cleanup
 
-    // TODO: @<name_here>
+    if (num_threads == 1){
+        visit_url(NULL);
+    }
+    else{
+        pthread_t threads[num_threads];
+        int threads_res[num_threads];
+        for (int i = 0; i < num_threads; i++){
+            threads_res[i] = pthread_create(threads + i, NULL, visit_url, NULL);
+        }
+
+        for (int i = 0; i < num_threads; i++){
+            if (threads_res[i] == 0){
+                pthread_join(threads[i], NULL);
+            }
+        }
+    }
+
+    //free(urls_frontier->stack);
+    //free(urls_frontier);
+
+    // TODO: @EVELYN
     // write png_urls.txt
     // perform optional write to LOGFILE
+    write_results(logfile_name);
 
     return 0;
 }
